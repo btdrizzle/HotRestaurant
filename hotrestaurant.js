552fdd2000fa6a52cdd7c0f35a56f5e9223e595b
btdrizzle <briandrake05@gmail.com>
2018-11-07 19:53:58 -0600

    express server

---
+++ hotrestaurant.js
@@ -0,0 +1,19 @@
+const reservations = [
+    {
+        name: "Brian and Vu Rool",
+        phoneNum: "555-555-5555",
+        eMail: "awesome@awesome.com",
+        uId: 12345
+    },
+];
+const waitList = [
+    {
+        name: "Brian and Vu Rool",
+        phoneNum: "555-555-5555",
+        eMail: "awesome@awesome.com",
+        uId: 12345
+    }
+];
+
+module.exports = {reservations,waitList};
+
